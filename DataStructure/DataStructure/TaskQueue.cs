67dd2040fb359b0370b73594b28bcc2bdb9e96e3
CalebTek <walexjackson@gmail.com>
2023-05-22 02:33:56 +0100

    Create class Task Queue

---
+++ DataStructure/DataStructure/TaskQueue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class TaskQueue<T> : SinglyLinkedList<T>
+    {
+        //private SinglyNode<T> nodeHead;
+        private SinglyNode<T> nodeTail;
+
+        public void Enqueue(T Rvalue)
+        {
+            var node = new SinglyNode<T> { Tvalue = Rvalue };
+            
+            if (nodeHead == null )
+            {
+                nodeHead = node;
+                nodeTail = node;
+            }
+            else
+            {
+                nodeTail.nextNode = node;
+                nodeTail = node;
+            }
+        }
+        
+    }
+}
