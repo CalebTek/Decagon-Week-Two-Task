4476789d96c4767723162049f248817686babe0b
CalebTek <walexjackson@gmail.com>
2023-05-22 01:05:53 +0100

    create Add method of class singly linked list

---
+++ DataStructure/DataStructure/SinglyLinkedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class SinglyNode<T>
+    {
+        public T Tvalue { get; set; }
+        public SinglyNode<T> nextNode { get; set; }
+    }
+    internal class SinglyLinkedList <T>
+    {
+        public SinglyNode<T> nodeHead;
+        private int listSize;
+
+
+        public int Add(T Rvalue)
+        {
+            var node = new SinglyNode<T> { Tvalue = Rvalue};
+            if (nodeHead == null)
+            {
+                nodeHead = node;
+            }
+            else
+            {
+                var currentValue = nodeHead;
+                while (!(currentValue.nextNode == null))
+                {
+                    currentValue = currentValue.nextNode;
+                }
+                currentValue.nextNode = node;
+            }
+            listSize++;
+            return listSize;
+        }
+        
+    }
+}
